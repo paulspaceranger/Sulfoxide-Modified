720d9eb486b13c50c472eb8e3f8817da494ab4ec
0_Void <106687374+0Void2391@users.noreply.github.com>
2024-10-25 17:46:06 +0200

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,2 @@
+getgenv().ui, getgenv().customrequire = loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/ui/ui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/modules/Remotespy.lua"))
