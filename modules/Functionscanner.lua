9a0d9206fc79d8b9a532dbe7b111a3200120d5e6
0_Void <106687374+0Void2391@users.noreply.github.com>
2025-01-13 21:28:25 +0100

    Update Functionscanner.lua

--- modules/Functionscanner.lua
+++ modules/Functionscanner.lua
@@ -1,12 +1,12 @@
-local luaencode = loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/modules/luaencode.lua"))()
-local contextmenu = loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/ui/controls/Contextmenu.lua"))()
+local luaencode = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/modules/luaencode.lua"))()
+local contextmenu = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/ui/controls/Contextmenu.lua"))()
 local function searchfunctions(query, mode, funcframe)
 if mode == "Name" then
     for i,v in pairs(getgc()) do
         if typeof(v) == "function" and not iscclosure(v) and not isourclosure(v) and debug.getinfo(v) and ((debug.getinfo(v).name and string.find(string.lower(debug.getinfo(v).name), string.lower(query))) or not debug.getinfo(v).name and string.find(string.lower(query), "unnamed")) then
             local newfuncframe = funcframe:Clone()
             newfuncframe.Parent = ui.Main.group.Functionscanner.Results
-            newfuncframe.Name.Text = debug.getinfo(v).name or "Unnamed function"
+            newfuncframe:FindFirstChild("Name").Text = debug.getinfo(v).name or "Unnamed function"
             newfuncframe.Protos.Text = tostring(#debug.getprotos(v))
             newfuncframe.Constants.Text = tostring(#debug.getconstants(v))
             newfuncframe.Upvalues.Text = tostring(#debug.getupvalues(v))
@@ -74,7 +74,7 @@ elseif mode == "Path" then
             if typeof(v) == "function" and not iscclosure(v) and not isourclosure(v) and debug.getinfo(v) and compareinstances(getfenv(v).script, instance) then --using getfenv because it returns an instance but i could loadstring the instance string returned by getinfo later on
             local newfuncframe = funcframe:Clone()
             newfuncframe.Parent = ui.Main.group.Functionscanner.Results
-            newfuncframe.Name.Text = debug.getinfo(v).name or "Unnamed function"
+            newfuncframe:FindFirstChild("Name").Text = debug.getinfo(v).name or "Unnamed function"
             newfuncframe.Protos.Text = tostring(#debug.getprotos(v))
             newfuncframe.Constants.Text = tostring(#debug.getconstants(v))
             newfuncframe.Upvalues.Text = tostring(#debug.getupvalues(v))
