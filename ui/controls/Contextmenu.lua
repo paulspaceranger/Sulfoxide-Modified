c18d3d151dcd4a697e8d472d475fde9718693248
0_Void <106687374+0Void2391@users.noreply.github.com>
2025-01-13 23:07:34 +0100

    Update Contextmenu.lua

--- ui/controls/Contextmenu.lua
+++ ui/controls/Contextmenu.lua
@@ -4,19 +4,13 @@ local userinputservice
 local Contextmenu = function(instance,content) --content structure example {{Text = "Decompile", Image = "rbxassetid", Func = function}}
 	instance.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton2 then
-			for _, obj in pairs(instance.Parent:GetChildren()) do
-				if obj:FindFirstChild("ContextMenu") then
-					obj.ZIndex = 1
-					obj.ContextMenu:Destroy()
-				end
-			end
+			if instance.Parent.Parent:FindFirstChild("ContextMenu") then instance.Parent.Parent:FindFirstChild("ContextMenu"):Destroy() end
 			local contextmenu = baseframe:Clone()
 			local mouseloc = uip:GetMouseLocation()
 			contextmenu.Visible = true
 			contextmenu.Parent = instance.Parent.Parent
 			contextmenu.Position = UDim2.new(0, mouseloc.X-instance.Parent.Parent.AbsolutePosition.X, 0, mouseloc.Y-instance.Parent.Parent.AbsolutePosition.Y-60)
 			contextmenu:WaitForChild("TextButton"):Destroy()--destroy the button that comems with the contextmenu
-			instance.ZIndex = 2
 			for i,v in pairs(content) do
 				local button = baseframe.TextButton:Clone()
 				button.Parent = contextmenu
@@ -29,7 +23,6 @@ local Contextmenu = function(instance,content) --content structure example {{Tex
 			end
 			local connection;connection = uip.InputEnded:Connect(function(input)
 				if input.UserInputType == Enum.UserInputType.MouseButton1 then
-					instance.ZIndex = 1
 					contextmenu:Destroy()
 					connection:Disconnect()
 				end
