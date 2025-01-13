64d77f569af88bb0c3356114511445c0861b3fcd
0_Void <106687374+0Void2391@users.noreply.github.com>
2025-01-13 20:46:20 +0100

    Update init.lua (prerelease-2)

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 if getgenv().ui then warn("already executed!") return end
 getgenv().ui, getgenv().customrequire = loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/ui/ui.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/modules/Remotespy.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/ui/modules/Functionscanner.lua"))()--load connection to the searchbar
