3f94d717e5556f856a105dadad912578f53b0446
0_Void <106687374+0Void2391@users.noreply.github.com>
2025-01-13 01:23:10 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
+if getgenv().ui then warn("already executed!") return end
 getgenv().ui, getgenv().customrequire = loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/ui/ui.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/0Void2391/Sulfoxide/refs/heads/main/modules/Remotespy.lua"))()
